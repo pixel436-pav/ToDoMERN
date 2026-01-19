805068dd1a0a594493a945eaff6ae812b8cb3305
pavneetSingh <pavneets436@gmail.com>
2026-01-19 21:04:27 +0530

    post request logic added

--- server/router/todo.router.js
+++ server/router/todo.router.js
@@ -1,4 +1,34 @@
 import express from 'express'
 import { Router } from 'express'
+import Todo from '../models/ToDo.js';
 
 const router = express.Router();
+
+
+router.post('/todo',async (req,res) => {
+  try {
+    const { text , completed } = req.body;
+    if (!text) {
+      return res.status(400).json({message: `text is required`})
+    }
+    // to create a new todo 
+    const newTodo = new Todo({
+      text : text,
+      completed : completed || false // default to false if not provided
+    })
+    const savedTodo = await   newTodo.save();
+
+    res.status(201).json({
+      message : "todo created succesfully",
+      todo: savedTodo
+    })
+    
+  } catch (error) {
+    res.status(500).json({message : error.message})
+  }
+
+});
+
+
+
+export default router;
