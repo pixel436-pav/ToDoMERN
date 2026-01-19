09726d804de484d7404ea3accff68a99a73245b4
pavneetSingh <pavneets436@gmail.com>
2026-01-19 20:34:00 +0530

    mongoDb connected

--- server/server.js
+++ server/server.js
@@ -1,13 +1,36 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
-const app = express();
+
 dotenv.config();
 
+const app = express();
+
 const PORT = process.env.PORT || 3001
+const mongoDbURL = process.env.MongoDbURL
 
 //Middleware to Parse JSON
 app.use(express.json());
+
+mongoose.connect(mongoDbURL)
+    .then(() => {
+      console.log(`MongoDb is Connected`)
+    })
+    .catch((error) => {
+      console.error(error)
+    }
+    )
+
+app.post('/',async (req,res) => {
+  try {
+
+    
+  } catch (error) {
+    res.status(500).json({message : error.message})
+  }
+}
+)
 
 app.listen(PORT,() => {
     
