bf42b2062f1de9636a26b1a2a9000180a4704ce3
pavneetSingh <pavneets436@gmail.com>
2026-01-20 23:38:55 +0530

    Started the Project

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
+import {
+  getAllTodo,
+  createTodo,
+  deleteTodo,
+  toggleTodo,
+} from './api.js';
 
-const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+function App(){
+
+  const[todos, setTodos]= useState([]);
+  const [loading, setLoading] =useState(true);
+
+  useEffect(() => {
+
+    fetchTodos();
+
+  },[])
+
+  const fetchTodos = async () => {
+    try {
+      const res = await getAllTodo();
+      setTodos(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+    finally{
+      setLoading(false)
+    }
+  }
+
 }
 
-export default App
 
