d2343b1c1739063d8e93f65442ee5fadb175c9d7
pavneetSingh <pavneets436@gmail.com>
2026-02-02 01:28:34 +0530

    added cors

--- server/server.js
+++ server/server.js
@@ -3,12 +3,14 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import Todo from './models/ToDo.js'
 import router from './router/todo.router.js';
+import cors from 'cors';
 
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors())
 const PORT = process.env.PORT || 3001
 const mongoDbURL = process.env.MongoDbURL
 
