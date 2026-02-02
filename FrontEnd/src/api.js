6009545e9a25f77a8f3e8e51b95d89b8c2e16ab7
pavneetSingh <pavneets436@gmail.com>
2026-02-03 01:42:07 +0530

    bugs fixed

--- FrontEnd/src/api.js
+++ FrontEnd/src/api.js
@@ -18,13 +18,18 @@ export const testConnection = async () => {
 export const getAllTodo = async () => {const response = await axios.get(API_URL)
     return response.data ;}
 
-export const createTodo = (data)=> axios.post(API_URL,data)
+export const createTodo = async (data)=> { const response = await axios.post(API_URL,data)
+    return response.data.todo // this will return the actual data not the axios response 
+}
 
-export const updateTodo = (id,updatedData) => axios.put(`${API_URL}/${id}`,updatedData)
+export const updateTodo = async (id,updatedData) => {const response = await axios.put(`${API_URL}/${id}`,updatedData)
+return response.data.todo}
 
-export const toggleTodo = (id)=>axios.patch(`${API_URL}/${id}/toggle`)
+export const toggleTodo = async (id)=>{const response = await axios.patch(`${API_URL}/${id}/toggle`)
+response.data.todo}
 
-export const getTodoById = (id) =>
-    axios.get(`${API_URL}/${id}`)
+export const getTodoById = async (id) => {const response = await axios.get(`${API_URL}/${id}`)
+response.data.todo}
 
-export const deleteTodo = (id) => axios.delete(`${API_URL}/${id}`)
+export const deleteTodo = async (id) => {const response = await axios.delete(`${API_URL}/${id}`)
+response.data}
