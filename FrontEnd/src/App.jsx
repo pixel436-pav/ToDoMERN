8a02eb4c9592f4aecd367ac4f0e47eb37fe59351
pavneetSingh <pavneets436@gmail.com>
2026-02-03 00:47:08 +0530

    Toggle Todo is added

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,9 +1,9 @@
-import react from 'react'
+import React from 'react'
 import { useState, useEffect } from 'react';
 import { getAllTodo, createTodo, updateTodo, toggleTodo, getTodoById, deleteTodo } from './api.js'
 
-function App ( ){
-const [todos,setTodos] = useState([]);
+function App ( ){ 
+    const [todos,setTodos] = useState([]);
 const [newTodo,setNewTodo] = useState(''); // here we are just getting text input from the user , but not any JSON object 
 const [loading,setLoading] = useState(true);
 const [error,setError] = useState(null);
@@ -33,21 +33,40 @@ const fetchTodos = async () =>{
     
   } finally{
     setLoading(false)
-  }
-
+  } 
 }
+
+  // Toggle Complete status
+const handleToggle = async (id) => {
+
+  try {
+    await toggleTodo(id)
+
+    setTodos(todos.map(todo => {
+      return todo._id === id ? {...todo,completed : !todo.completed} : todo
+    }))
+
+  } catch (error) {
+    console.error('Error Message: ', error)
+    fetchTodos(); // this will refresh on error
+  }
+  
+}
+
 
 return (
 
   <div>
     <button onClick={fetchTodos}>retry</button>
     {todos.length === 0? ( <p>No Todos Yet -Add Your first One</p> ):( <ul>{todos.map(todo=> ( 
-      <li key={todo._id} ><input type="checkbox" checked= {todo.completed || false }/> <span> {todo.text} </span> </li> ) 
+      <li key={todo._id} ><input type="checkbox" checked= {todo.completed || false } onChange={()=> handleToggle(todo._id)} /> <span> {todo.text} </span> </li> ) 
       )}</ul> )}
 
   </div>
 
 )
-}
+
+} 
+
 
 export default App;
